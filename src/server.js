1b0da609600b21d5c6c1b4b4d7bfbd380316d671
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2023-10-03 16:56:09 +0700

    feat: how to use mySQL with NodeJs

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const { render } = require('ejs');
 const express = require('express');
 
+const mysql = require('mysql2');
+
 require('dotenv').config();
 
 const configViewEngine = require('./config/viewEngine');
@@ -16,6 +18,20 @@ configViewEngine(app);
 
 // route
 app.use('/', webRoutes);
+
+// test connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307, // default: 3306
+    user: 'root', // default password: empty
+    password: '123456',
+    database: 'hoidanit',
+});
+
+connection.query('select * from Users u', function (err, results, fields) {
+    console.log('===> results = ', results);
+    console.log('===> fields = ', fields);
+});
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening at ${port}`);
