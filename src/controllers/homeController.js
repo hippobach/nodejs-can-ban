3502d5d5cf4cbc6eb26fc5d535d515ba5f7fa430
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2023-10-14 21:43:56 +0700

    feat: design view edit user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -28,8 +28,13 @@ const getCreatePage = (req, res) => {
     res.render('create.ejs');
 };
 
+const getUpdatePage = (req, res) => {
+    res.render('edit.ejs');
+};
+
 module.exports = {
     getHomePage,
     postCreateUser,
     getCreatePage,
+    getUpdatePage,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getHomePage, postCreateUser, getCreatePage } = require('../controllers/homeController');
+const { getHomePage, postCreateUser, getCreatePage, getUpdatePage } = require('../controllers/homeController');
 
 router.get('/', getHomePage);
 router.get('/create', getCreatePage);
 
 router.post('/create-user', postCreateUser);
+
+router.get('/update', getUpdatePage);
 
 module.exports = router;
